9080476b3a460d21afa0b0aea7a41214ba0575f5
eric-shipsit <EricL-swe@gmail.com>
2025-05-11 23:10:12 -0700

    Image issue fixes

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: [
-      'lh3.googleusercontent.com', // Google profile images
-      'avatars.githubusercontent.com', // GitHub avatars if needed
-      // add any other domains here
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com',
     ],
   },
 };
