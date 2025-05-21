d8d483dabec7af338156314c60106cfa8677f711
eric-shipsit <EricL-swe@gmail.com>
2025-05-20 17:45:16 -0700

    Saving old AuthForm and replacing with MVP version

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -1,76 +1,13 @@
 // src/app/(site)/components/AuthForm.tsx
 "use client";
-
-import Button from "@/app/components/buttons/Button";
-import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import AuthSocialButton from "./AuthSocialButton";
-import axios from "axios";
 import { signIn } from "next-auth/react";
 import { toast } from "react-hot-toast";
-import router from "next/router";
-
-type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
-  const [variant, setVariant] = useState<Variant>("LOGIN");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const toggleVariant = useCallback(() => {
-    if (variant === "LOGIN") {
-      setVariant("REGISTER");
-    } else {
-      setVariant("LOGIN");
-    }
-  }, [variant]);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FieldValues>({
-    defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-    },
-  });
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setIsLoading(true);
-    if (variant === "REGISTER") {
-      axios
-        .post("/api/register", data)
-        .then(() => signIn("credentials", data))
-        .catch(() => toast.error("Something went wrong!"))
-        .finally(() => setIsLoading(false));
-    }
-    if (variant === "LOGIN") {
-      // Login
-      // await signIn('credentials' , { <---- await is saying this is a promise (pauses async function until this is fulfilled or rejected)
-      signIn("credentials", {
-        email: data.email,
-        password: data.password,
-        // ...data, <--- this is the same as above
-        redirect: false,
-      })
-        .then((callback) => {
-          if (callback?.error) {
-            toast.error("Invalid credentials");
-          }
-          if (callback?.ok && !callback?.error) {
-            toast.success("Logged in!");
-            router.push("/dashboard");
-          }
-        })
-        .finally(() => setIsLoading(false));
-    }
-  };
 
   const socialAction = (action: string) => {
-    setIsLoading(true);
     // await signIn(action, {redirect: false});
     signIn(action, { redirect: false })
       .then((callback) => {
@@ -80,81 +17,20 @@ const AuthForm = () => {
         if (callback?.ok && !callback?.error) {
           toast.success("Logged in!");
         }
-      })
-      .finally(() => setIsLoading(false));
+      });
   };
 
   return (
     <div className="mt-8 w-full sm:mx-auto sm:max-w-md">
-      <div className="sm:bg-white px-4 py-8 sm:shadow sm:rounded-lg sm:px-10">
-        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          {variant === "REGISTER" && (
-            <Input
-              id="name"
-              label="Name"
-              register={register}
-              errors={errors}
-              disabled={isLoading}
-            />
-          )}
-          <Input
-            id="email"
-            label="Email address"
-            type="email"
-            register={register}
-            errors={errors}
-            disabled={isLoading}
-          />
-          <Input
-            id="password"
-            label="Password"
-            type="password"
-            register={register}
-            errors={errors}
-            disabled={isLoading}
-          />
-          <div>
-            <Button disabled={isLoading} fullWidth type="submit">
-              {variant === "LOGIN" ? "Sign in" : "Register"}
-            </Button>
-          </div>
-        </form>
-        <div className="mt-6">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-300" />
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-gray-50 sm:bg-white px-2 text-gray-500">
-                Or continue with
-              </span>
-            </div>
-          </div>
-          <div className="mt-6 flex gap-2">
-            <AuthSocialButton
-              icon={BsGithub}
-              onClick={() => socialAction("github")}
-            />
-            <AuthSocialButton
-              icon={BsGoogle}
-              onClick={() => socialAction("google")}
-            />
-          </div>
-        </div>
-        <div
-          className="
-          flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500
-        "
-        >
-          <div>
-            {variant === "LOGIN"
-              ? "New to Panda Pocket?"
-              : "Already have an account?"}
-          </div>
-          <div onClick={toggleVariant} className="underline cursor-pointer">
-            {variant === "LOGIN" ? "Create an account" : "Log in"}
-          </div>
-        </div>
+      <div className="mt-0 flex gap-2">
+        <AuthSocialButton
+          icon={BsGithub}
+          onClick={() => socialAction("github")}
+        />
+        <AuthSocialButton
+          icon={BsGoogle}
+          onClick={() => socialAction("google")}
+        />
       </div>
     </div>
   );
